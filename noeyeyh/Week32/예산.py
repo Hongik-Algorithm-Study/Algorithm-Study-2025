89b64fbaa98571c9c76e56fa474e6ae1f114dc58
안혜연 <ahy020606@naver.com>
2025-04-05 16:11:10 +0900

    Feat: 32주차_예산 완료

---
+++ noeyeyh/Week32/예산.py
@@ -0,0 +1,12 @@
+def solution(d, budget):
+    sum = 0
+    count = 0
+    d.sort()
+    
+    for i in d:
+        if sum + i > budget:
+            break
+        sum += i
+        count += 1
+        
+    return count
