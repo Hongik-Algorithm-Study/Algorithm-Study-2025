940e5518817e3c939d07ccbe27929a48e6e276d4
이예지 <oyatpeach@naver.com>
2025-06-25 20:55:21 +0900

    [feat] 힙 - 디스크 컨트롤러 완료

---
+++ oyatplum/Week35/알고리즘/디스크 컨트롤러.py
@@ -0,0 +1,22 @@
+import heapq
+def solution(jobs):
+    answer = 0
+    start = -1
+    now = 0
+    count = 0
+    heap = []
+
+    while count < len(jobs):
+        for job in jobs:
+            if start < job[0] <= now:
+                heapq.heappush(heap, [job[1], job[0]])
+
+        if heap:
+            current = heapq.heappop(heap)
+            start = now
+            now += current[0]
+            count += 1
+            answer += now - current[1]
+        else:
+            now += 1
+    return answer // len(jobs)
