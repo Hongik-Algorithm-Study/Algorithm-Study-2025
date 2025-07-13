a6f0445366532323f764d793c8ab8e70d8af96cb
이예지 <oyatpeach@naver.com>
2025-07-13 15:19:24 +0900

    [feat] 과제 - 피로도 완료

---
+++ oyatplum/Week37/과제/피로도.py
@@ -0,0 +1,16 @@
+from itertools import permutations
+def solution(k, dungeons):
+    answer = -1
+    permutes = list(permutations(dungeons, len(dungeons)))
+
+    for perm in permutes:
+        cur = k
+        total = 0
+        for p in perm:
+            if cur >= p[0] and cur - p[1] >= 0:
+                cur -= p[1]
+                total += 1
+        if total > answer:
+            answer = total
+
+    return answer
