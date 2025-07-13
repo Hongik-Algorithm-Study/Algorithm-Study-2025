e430480a5bb2adada39105974dc5284402965ccc
이예지 <oyatpeach@naver.com>
2025-07-13 15:18:38 +0900

    [feat] 과제 - 카펫 완료

---
+++ oyatplum/Week37/과제/카펫.py
@@ -0,0 +1,19 @@
+def solution(brown, yellow):
+    answer = []
+    total = brown + yellow
+
+    for i in range(1, total+1):
+        if total % i == 0:
+            w = i
+            h = total // i
+
+            if (w-2) * (h-2) == yellow:
+                if w >= h:
+                    answer.append(w)
+                    answer.append(h)
+                else:
+                    answer.append(h)
+                    answer.append(w)
+                break
+
+    return answer
