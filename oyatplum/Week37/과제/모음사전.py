d1ce4f4ef2746444ea87c336c96f41a5bdf5fdc7
이예지 <oyatpeach@naver.com>
2025-07-13 15:19:46 +0900

    [feat] 과제 - 모음사전 완료

---
+++ oyatplum/Week37/과제/모음사전.py
@@ -0,0 +1,33 @@
+def solution(word):
+    answer = 0
+    alphaList = ['A', 'E', 'I', 'O', 'U']
+    flag = 0
+
+    def dfs(alpha, cur):
+        cur += alpha
+        nonlocal answer
+        nonlocal flag
+
+        if flag == 1:
+            return
+
+        answer += 1
+
+        if cur == word:
+            flag = 1
+            return
+
+        if len(cur) == 5:
+            return
+
+        for alpha in alphaList:
+            dfs(alpha, cur)
+
+
+    for alpha in alphaList:
+        if flag == 0:
+            dfs(alpha, '')
+        else:
+            break
+
+    return answer
