905d6a9eff908fc3a3bdac365864697cfa502345
이예지 <oyatpeach@naver.com>
2025-07-19 15:27:36 +0900

    [feat] 과제 - 타겟넘버 완료

---
+++ oyatplum/Week38/과제/타겟넘버.py
@@ -0,0 +1,22 @@
+def solution(numbers, target):
+    answer = 0
+
+    def dfs(idx, total):
+        nonlocal answer
+
+        if idx == len(numbers) - 1:
+            if total + numbers[idx] == target:
+                answer += 1
+                return
+            if total - numbers[idx] == target:
+                answer += 1
+                return
+            else:
+                return
+
+        dfs(idx+1, total + numbers[idx])
+        dfs(idx+1, total - numbers[idx])
+
+    dfs(0, 0)
+
+    return answer
